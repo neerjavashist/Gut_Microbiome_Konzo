1e492c4bd016909d12ecdf85ced609407affa501
neerjavashist <neerjavashist@ucla.edu>
2020-09-04 13:11:30 -0400

    Update Code.R

--- Code.R
+++ Code.R
@@ -1,1 +1,63 @@
+### All packages that may be required
+library("patchwork")
+library("ggpubr")
+library("devtools")
+library("picante")
+library("TSA")
+library("nortest")
+library("multcomp")
+library("car")
+library("microbiome")
+library("mvabund")
+library("MASS")
+library("geepack")
+library("doBy")
+library("lattice")
+library("MuMIn")
+library("phangorn")
+library("DESeq2")
+library("FSA")
+library("phyloseq")
+library("microbiome")
+library("tidyverse")
+library("ggplot2")
+library("rentrez")
+library("XML")
+library("ggrepel")
+library("plyr")
+library("vegan")
+library("DESeq2")
+library("reshape2")
+library("colorspace")
+library("zoo")
+library("magrittr")
+library("tidyr")
+library("cvcqv")
+library("raster")
+
+### Konzo Meta Data
+
+### Read Count to Relative Abundance
+#Bacteria Phylum
+#Bacteria Class
+#Bacteria Order
+#Bacteria Family
+#Bacteria Genus
+#Bacteria Species
+
+### Estimate Richness
+
+### Beta Diversity using Bray-Curtis for Bacteria Genus
+
+### Mann Whitney-Wilcox Test (with BH correction)
+#Bacteria Phylum
+#Bacteria Class
+#Bacteria Order
+#Bacteria Family
+#Bacteria Genus
+#Bacteria Species
+
+
+
+
 
