b7d35f28f34f8f4e7cc69c16b94afdf059b60bcc
neerjavashist <neerjavashist@ucla.edu>
2021-03-03 14:58:34 -0500

    Update Fig.R

--- Fig.R
+++ Fig.R
@@ -41,7 +41,7 @@ for(i in 1:sum100)
           }
 }
 
-G.tr.DF$Status <- factor(G.tr.DF$Status, levels = c("Kinshasa", "Masimanimba","Kahemba_Control_NonIntervention", "Kahemba_Control_Intervention"))
+G.tr.DF$Status <- factor(G.tr.DF$Status, levels = c("Kinshasa", "Masimanimba","Unaffected_Low_Prevalence_Zone", "Unaffected_High_Prevalence_Zone"))
 
 #G.tr.DF now has the rel abund data, with eigen values for each axis as additional columns so correlation can be done between genus relative abundance and PCoA Axis values to see which genus correlated the best with each Axis values
                                              
@@ -73,36 +73,38 @@ for (i in a:ncol(G.tr.DF))
 #Axis 1 Prevotella
 #Axis 2 Lachnoclostridium
 
+#(plot.margin=unit(c(t,r,b,l))
+
 a1 <- ggplot(G.tr.DF, aes(x = Axis.1, y = Prevotella)) +
-    geom_point(aes(color = factor(Status)), size = 1, stroke = 0, shape = 16) + theme_classic() + ylab("Prev.") + theme(axis.title.x = element_blank(), axis.text.x = element_text(size = 7))
-a1 <- a1 + scale_color_manual(labels = SL, values = geography_color) + theme(legend.position="none", panel.border = element_rect(colour = "black", fill=NA, size=0.5)) + theme (axis.title.y = element_text(size = 7, face = "italic"), axis.text.y = element_text(size = 7))  
-a1 <- a1 + geom_smooth(method=lm, color = "black", size = 0.5) + theme(plot.margin=unit(c(0.15,0.15,0.25,0.15), "lines")) + scale_y_continuous(breaks = seq(-0.1, 0.4, by = 0.2))
+    geom_point(aes(color = factor(Status)), size = 0.75, stroke = 0, shape = 16) + theme_classic() + ylab("Prev.") + theme(axis.title.x = element_blank(), axis.text.x = element_text(size = 5))
+a1 <- a1 + scale_color_manual(labels = SL, values = geography_color) + theme(legend.position="none", panel.border = element_rect(colour = "black", fill=NA, size=0.5)) + theme (axis.title.y = element_text(size = 5, face = "italic"), axis.text.y = element_text(size = 5))  
+a1 <- a1 + geom_smooth(method=lm, color = "black", size = 0.5) + theme(plot.margin=unit(c(0.025,0.1,0.1,0.1), "lines")) + scale_y_continuous(breaks = seq(-0.1, 0.4, by = 0.2))
 #a1 <- a1 + stat_cor(method = "spearman", size = 5) 
 
 a2 <- ggplot(G.tr.DF, aes(x = Lachnoclostridium, y = Axis.2)) +
-    geom_point(aes(color = factor(Status)), size = 1, stroke = 0, shape = 16) + theme_classic() + xlab("Lach.") + theme(axis.title.y = element_blank(), axis.text.y = element_text(size = 7))
-a2 <- a2 + scale_color_manual(labels = SL, values = geography_color) + theme(legend.position="none", panel.border = element_rect(colour = "black", fill=NA, size=0.5)) + theme (axis.title.x = element_text(size = 7, face = "italic"), axis.text.x = element_text(size = 7))  
-a2 <- a2 + scale_y_continuous(position = "right") + scale_x_continuous(position = "top", breaks = seq(0.01, 0.02, by = 0.01))
-a2 <- a2 + geom_smooth(method=lm, color = "black", size = 0.5) + theme(plot.margin=unit(c(0.15,0.125,0.15,0.15), "lines"))
+    geom_point(aes(color = factor(Status)), size = 0.75, stroke = 0, shape = 16) + theme_classic() + xlab("Lach.") + theme(axis.title.y = element_blank(), axis.text.y = element_text(size = 5))
+a2 <- a2 + scale_color_manual(labels = SL, values = geography_color) + theme(legend.position="none", panel.border = element_rect(colour = "black", fill=NA, size=0.5)) + theme (axis.title.x = element_text(size = 5, face = "italic"), axis.text.x = element_text(size = 5))  
+a2 <- a2 + scale_y_continuous(position = "right") + scale_x_continuous(position = "top", breaks = seq(0.01, 0.025, by = 0.015))
+a2 <- a2 + geom_smooth(method=lm, color = "black", size = 0.5) + theme(plot.margin=unit(c(0.1,0,0.1,0.025), "lines"))
 
 p1 = plot_ordination(Geography.G.tr, ordinate(Geography.G.tr, method="PCoA", dist="bray"), type="samples", color="Status") +
-  geom_point(size = 2, stroke = 0, shape = 16)
+  geom_point(size = 1, stroke = 0, shape = 16)
 p1$layers <- p1$layers[-1]
 #p1 <- as_ggplot(p1)
 PGB <- p1 + 
   labs(color = "Groups")+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), 
                                 axis.line = element_line(colour = "black")) + scale_color_manual(values = geography_color, labels = SSSL)+
-  theme(legend.title=element_blank(), legend.background = element_rect(colour = NA, fill = NA), legend.margin=margin(c(0,0,0,0))) + theme (legend.key = element_rect(colour = NA, fill = NA ), panel.border = element_rect(colour = "black", fill=NA, size=0.5)) + theme(legend.key.size = unit(.1, "cm")) + theme(legend.text = element_text(size=7)) +
-  theme(axis.title.y = element_text(size = 7), axis.title.x = element_text(size = 7), axis.text.y = element_text(size = 7), axis.text.x = element_text(size = 7))
+  theme(legend.title=element_blank(), legend.background = element_rect(colour = NA, fill = NA), legend.margin=margin(c(0,0,0,0))) + theme (legend.key = element_rect(colour = NA, fill = NA ), panel.border = element_rect(colour = "black", fill=NA, size=0.5)) + theme(legend.key.size = unit(.1, "cm")) + theme(legend.text = element_text(size=5)) +
+  theme(axis.title.y = element_text(size = 5), axis.title.x = element_text(size = 5), axis.text.y = element_text(size = 5), axis.text.x = element_text(size = 5))
 
 l <- get_legend(PGB)
 l <- as_ggplot(l)
 l <- l + theme(plot.margin=unit(c(-1,0,0,-1), "lines"))
 
-PGBt <- PGB + stat_ellipse(type = "t") + scale_x_continuous(position = "top") + theme(plot.margin=unit(c(0.15,0.15,0.15,0.15), "lines"))
+PGBt <- PGB + stat_ellipse(type = "t") + scale_x_continuous(position = "top") + theme(plot.margin=unit(c(0.1,0.1,0.1,0.1), "lines"))
 PGBt <- PGBt + theme(legend.position="none")
-PGBt <- PGBt + annotate("text", x = -0.43, y = -0.42, label = expression(paste("p = 9.999x",10^-5)), size = 2.5)
-PGBt <- ggarrange(PGBt,labels = c("A"),font.label = list(size = 7))
+PGBt <- PGBt + annotate("text", x = -0.47, y = -0.42, label = expression(paste("p = 1x",10^-5)), size = 2)
+PGBt <- ggarrange(PGBt,labels = c("A"),font.label = list(size = 5))
 
 PGB <- PGB + scale_x_continuous(position = "top") + theme(plot.margin=unit(c(0.15,0.15,0.15,0.15), "lines")) + theme(legend.position="none")
 
@@ -112,9 +114,125 @@ G <- arrangeGrob(PGBt, a1,                               # bar plot spaning two 
              ncol = 2, nrow = 2,
              layout_matrix = rbind(c(1,1,1,3), c(1,1,1,3), c(1,1,1,3), c(2, 2, 2, 4)))
 
-tiff(filename = "Overall_Geography_Genus_PCoA_Corr.tiff", width = 3.5, height = 3.5, units = "in", res = 600)
+tiff(filename = "Overall_Geography_Genus_PCoA_Corr.tiff", width = 2.5, height = 2.5, units = "in", res = 600)
 ggarrange(as_ggplot(G))
 dev.off()
 
 
 #PCoA KO
+b <- ordinate(Geography.KO.tr, method="PCoA", dist="bray")
+b.DF <- as.data.frame(b$vectors)
+e.DF <- as.data.frame(b$values$Relative_eig)
+e.DF["Percent"] <- (b$values$Relative_eig*100)
+
+#Find out how many Axis needed to reach >= 1 or 100% variance
+esum = 0
+sum100 = 0
+for (i in 1:nrow(e.DF))
+{
+  esum = esum + e.DF[i,1]
+  if(esum >= 1)
+  {
+    sum100 = i
+    break
+  }
+}
+
+K <- Geography.KO.tr
+K.tr.DF <- as.data.frame(t(K@otu_table))
+
+n = ncol(K.tr.DF)
+
+K.tr.DF["Status"] <- NA
+
+#Add the needed Axis Values
+
+for(i in 1:sum100)
+{
+  K.tr.DF[colnames(b.DF[i])] <- NA
+          for (j in 1:nrow(K.tr.DF))
+          {
+            K.tr.DF[j,colnames(b.DF[i])] <- b.DF[rownames(K.tr.DF[j,]),i]
+          }
+}
+
+K.tr.DF$Status <- factor(K.tr.DF$Status, levels = c("Kinshasa", "Masimanimba","Unaffected_Low_Prevalence_Zone", "Unaffected_High_Prevalence_Zone"))
+
+#K.tr.DF now has the rel abund data, with eigen values for each axis as additional columns so correlation can be done between KO relative abundance and PCoA Axis values to see which genus correlated the best with each Axis values
+                                             
+for (i in 1:nrow(K.tr.DF))
+  {K.tr.DF[i,]$Status <- Geography.KO.tr@sam_data[rownames(K.tr.DF[i,]),]$Status
+  }
+
+a = n+2 #moves starting position to Axis.1 column
+#for loop to correlate each KO with each Axis.1 using the spearman method                                                                                          
+for (i in a:ncol(K.tr.DF))
+{
+  Cor <- matrix(nrow = n,  ncol = 3)
+  colnames(Cor) <- c(paste(colnames(K.tr.DF[i])), "spearman cor", "p-value")
+  for (j in 1:n)
+  {
+    cor <- cor.test(K.tr.DF[,j], K.tr.DF[,i], method=c("spearman"))
+    Cor[j,1] = colnames(K.tr.DF[j])
+    Cor[j,2] = as.numeric(cor$estimate)
+    Cor[j,3] = as.numeric(cor$p.value)
+  }
+  write.csv(Cor, file = paste("Geography_KO_",colnames(K.tr.DF[i]),"_Correlation.csv", sep = ""))
+  Cor <- data.frame(Cor, row.names = TRUE)
+  Cor.f <- subset(Cor, rownames(Cor) %in% f_0.0001)                                       
+  write.csv(Cor.f, file = paste("Geography_KO_f_0.0001_",colnames(K.tr.DF[i]),"_Correlation.csv", sep = ""))
+}
+
+#Plot most correlated with Axis 1 and Axis 2                                             
+#Correlation Plot
+#Axis 1 K15977
+#Axis 2 K01190
+
+a1 <- ggplot(K.tr.DF, aes(x = Axis.1, y = K15977)) +
+    geom_point(aes(color = factor(Status)), size = 0.75, stroke = 0, shape = 16) + theme_classic() + ylab("K15977") + theme(axis.title.x = element_blank(), axis.text.x = element_text(size = 5))
+a1 <- a1 + scale_color_manual(labels = SL, values = geography_color) + theme(legend.position="none", panel.border = element_rect(colour = "black", fill=NA, size=0.5)) + theme (axis.title.y = element_text(size = 5), axis.text.y = element_text(size = 5))  
+a1 <- a1 + geom_smooth(method=lm, color = "black", size = 0.5) + theme(plot.margin=unit(c(0.025,0.1,0.1,0.1), "lines")) #+ scale_y_continuous(breaks = seq(-0.1, 0.4, by = 0.2))
+#a1 <- a1 + stat_cor(method = "spearman", size = 5) 
+
+a2 <- ggplot(K.tr.DF, aes(x = K01190, y = Axis.2)) +
+    geom_point(aes(color = factor(Status)), size = 0.75, stroke = 0, shape = 16) + theme_classic() + xlab("K01190") + theme(axis.title.y = element_blank(), axis.text.y = element_text(size = 5))
+a2 <- a2 + scale_color_manual(labels = SL, values = geography_color) + theme(legend.position="none", panel.border = element_rect(colour = "black", fill=NA, size=0.5)) + theme (axis.title.x = element_text(size = 5), axis.text.x = element_text(size = 5))  
+a2 <- a2 + scale_y_continuous(position = "right") + scale_x_continuous(position = "top", breaks = seq(0.008, 0.016, by = 0.016))
+a2 <- a2 + geom_smooth(method=lm, color = "black", size = 0.5) + theme(plot.margin=unit(c(0.1,0,0.1,0.025), "lines"))
+
+p1 = plot_ordination(Geography.KO.tr, ordinate(Geography.KO.tr, method="PCoA", dist="bray"), type="samples", color="Status") +
+  geom_point(size = 1, stroke = 0, shape = 16)
+p1$layers <- p1$layers[-1]
+#p1 <- as_ggplot(p1)
+PGB <- p1 + 
+  labs(color = "Groups")+ theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), 
+                                axis.line = element_line(colour = "black")) + scale_color_manual(values = geography_color, labels = SSSL)+
+  theme(legend.title=element_blank(), legend.background = element_rect(colour = NA, fill = NA), legend.margin=margin(c(0,0,0,0))) + theme (legend.key = element_rect(colour = NA, fill = NA ), panel.border = element_rect(colour = "black", fill=NA, size=0.5)) + theme(legend.key.size = unit(.1, "cm")) + theme(legend.text = element_text(size=5)) +
+  theme(axis.title.y = element_text(size = 5), axis.title.x = element_text(size = 5), axis.text.y = element_text(size = 5), axis.text.x = element_text(size = 5))
+
+l <- get_legend(PGB)
+l <- as_ggplot(l)
+l <- l + theme(plot.margin=unit(c(-1,0,0,-1), "lines"))
+
+PGBt <- PGB + stat_ellipse(type = "t") + scale_x_continuous(position = "top") + theme(plot.margin=unit(c(0.1,0.1,0.1,0.1), "lines"))
+PGBt <- PGBt + theme(legend.position="none")
+PGBt <- PGBt + annotate("text", x = -0.25, y = -0.4, label = expression(paste("p = 1x",10^-5)), size = 1.75)
+PGBt <- ggarrange(PGBt,labels = c("B"),font.label = list(size = 5))
+
+PGB <- PGB + scale_x_continuous(position = "top") + theme(plot.margin=unit(c(0.15,0.15,0.15,0.15), "lines")) + theme(legend.position="none")
+
+
+G2 <- arrangeGrob(PGBt, a1,                               # bar plot spaning two columns
+             a2, l,                               # box plot and scatter plot
+             ncol = 2, nrow = 2,
+             layout_matrix = rbind(c(1,1,1,3), c(1,1,1,3), c(1,1,1,3), c(2, 2, 2, 4)))
+
+tiff(filename = "Overall_Geography_KO_PCoA_Corr.tiff", width = 2.5, height = 2.5, units = "in", res = 600)
+ggarrange(as_ggplot(G2))
+dev.off()
+
+tiff(filename = "Overall_Geography_Genus_KO_PCoA_Corr.tiff", width = 2.5, height = 5, units = "in", res = 600)
+ggarrange(as_ggplot(G), as_ggplot(G2), ncol = 1, nrow = 2)
+dev.off()
+
+
